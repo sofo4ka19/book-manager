2c051a8aa498e9d5c2bc9013e10c890fb7bed7b9
Sofiia Broiako <yroha0013@gmail.com>
2024-10-14 16:00:49 +0300

    add function to set Book

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -2,15 +2,21 @@ export default class Book {
     constructor(
         private title: string,
         private author: string,
+        private genre: string,
         private isbn: number,
         private imageUrl: string,
         private rate: number,
         private myAssesment: number=-1,
-    ){}
-    public get info() : {title: string, author: string, isbn: number, imageUrl: string, rate: number}{
+    ){
+        if(myAssesment>=0 && myAssesment<=5){
+            this.myAssesment=myAssesment;
+        }
+    }
+    public get info() : {title: string, author: string, genre: string, isbn: number, imageUrl: string, rate: number}{
         return{
             title: this.title,
             author: this.author,
+            genre: this.genre,
             isbn: this.isbn,
             imageUrl: this.imageUrl,
             rate: this.rate
@@ -29,5 +35,13 @@ export default class Book {
             this.myRate;
         }
         return this.myAssesment;
+    }
+    public set info(bookInfo:{title: string, author: string, genre: string, isbn: number, imageUrl: string, rate: number}){
+        this.title=bookInfo.title;
+        this.author=bookInfo.author;
+        this.genre=bookInfo.genre;
+        this.isbn=bookInfo.isbn;
+        this.imageUrl=bookInfo.imageUrl;
+        this.rate=bookInfo.rate;
     }
 }
